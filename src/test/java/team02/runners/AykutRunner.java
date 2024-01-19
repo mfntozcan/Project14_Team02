70bda3894d29af67cd41aefac903d83e96d156b3
Aykut Umut <aykutumt@gmail.com>
2024-01-19 22:17:59 +0000

    last version

---
+++ src/test/java/team02/runners/AykutRunner.java
@@ -0,0 +1,16 @@
+package team02.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:raporlar/CucumberRapor.html",
+        features = "src/test/resources",
+        glue ={"team02/stepDefinition", "team02/hooks"},
+        tags = "@US021_TC04",
+        dryRun = false
+)
+public class AykutRunner {
+}
--- src/test/java/team02/runners/Runner.java
+++ src/test/java/team02/runners/Runner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
         plugin = "html:raporlar/CucumberRapor.html",
         features = "src/test/resources",
         glue ={"team02/stepDefinition", "team02/hooks"},
-        tags = "@US021_TC07",
+        tags = "@API",
         dryRun = false
 )
-public class RunnerAykut {
+public class Runner {
 }
